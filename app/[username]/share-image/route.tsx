6b25a41ecc070ec9f75526fae4cda293155d6b60
Nate McGrady <nathanjohnmcgrady@gmail.com>
2026-02-24 09:48:24 -0600

    Enhance share-image route: add objectFit style to image components for improved layout consistency in both dense and standard views.

--- app/[username]/share-image/route.tsx
+++ app/[username]/share-image/route.tsx
@@ -247,6 +247,7 @@ export async function GET(request: Request, { params }: { params: Promise<{ user
                     style={{
                       width: isDenseLayout ? 20 : 32,
                       height: isDenseLayout ? 20 : 32,
+                      objectFit: 'contain',
                     }}
                   />
                 </div>
@@ -313,6 +314,7 @@ export async function GET(request: Request, { params }: { params: Promise<{ user
                       style={{
                         width: isDenseLayout ? 20 : 32,
                         height: isDenseLayout ? 20 : 32,
+                        objectFit: 'contain',
                       }}
                     />
                   </div>
